4fba63562c204183a1290b6f857412433e567666
Thomatito <zarama.minecraft@gmail.com>
2024-06-05 14:57:09 -0300

    botao

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,20 +1,41 @@
-import { createContext, useEffect, useState, useContext } from "react";
+import React, { createContext, useEffect, useState, useContext } from "react";
 import { useColorScheme } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  // detecta qual é o tema do sistema
-
   const scheme = useColorScheme();
- 
   const [isDarkTheme, setIsDarkTheme] = useState(scheme === "dark");
 
-  // ele vai ficar escutando a variável responsável
-
+  // Carregar a preferência do tema do AsyncStorage quando o componente monta
   useEffect(() => {
-    setIsDarkTheme(scheme === "dark");
+    const loadTheme = async () => {
+      try {
+        const themeValue = await AsyncStorage.getItem("isDarkTheme");
+        if (themeValue !== null) {
+          setIsDarkTheme(JSON.parse(themeValue));
+        } else {
+          setIsDarkTheme(scheme === "dark");
+        }
+      } catch (error) {
+        console.error("Erro ao carregar o tema", error);
+      }
+    };
+    loadTheme();
   }, [scheme]);
+
+  // Salvar a preferência do tema no AsyncStorage quando isDarkTheme muda
+  useEffect(() => {
+    const saveTheme = async () => {
+      try {
+        await AsyncStorage.setItem("isDarkTheme", JSON.stringify(isDarkTheme));
+      } catch (error) {
+        console.error("Erro ao salvar o tema", error);
+      }
+    };
+    saveTheme();
+  }, [isDarkTheme]);
 
   return (
     <ThemeContext.Provider
